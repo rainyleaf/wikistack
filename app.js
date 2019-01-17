9efee1fa14cbc335b9366091ba51b7a1b269ef9b
Lydie Toussaint <lydietoussaint123@gmail.com>
2019-01-17 13:04:52 -0500

    updates

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const morgan = require('morgan');
 const express = require('express');
-//const Sequelize = require('sequelize');
 const layout = require('./views/layout')
-//const db = new Sequelize('')
+const {db} = require('./models');
 
 const app = express();
+db.authenticate().then(()=>{
+    console.log('connected to the database');
+})
+
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + "/public"));
